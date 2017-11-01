0346da9fb4166d80caebe90d0c14d5ff10e538e3
tml544809393 <544809393@qq.com>
2017-11-01 17:45:36 +0800

    change

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="test">
+    {{msg}}:{{age}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      age:23
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.test{
+	background:red;
+}
+</style>
